95a29415097a8a1f240ee60f62282e1a424ece09
Ekadarshini Routray <100223975+Ekadarshini7272@users.noreply.github.com>
2026-04-23 12:25:23 +0530

    product page modification

--- src/components/container/product.js
+++ src/components/container/product.js
@@ -4,70 +4,38 @@ import Image from "next/image";
 import Link from "next/link";
 import { FiArrowUpRight } from "react-icons/fi";
 import styles from "./product.module.scss";
-
 import product1 from "../../../public/images/product1.png";
 import product2 from "../../../public/images/product2.png";
-
 const Product = () => {
   return (
     <section className={styles.homeSection}>
       <Container>
-
         <Row className="justify-content-center">
-
-          {/* PRODUCT 1 */}
-          <Col lg={10}>
-            <div className={styles.cardWrapper}>
-
-              <div className={styles.cardHeader}>
-                <span className={styles.productLabel}>Product 1</span>
-
-                <Link href="#" className={styles.arrowBtn}>
-                  <FiArrowUpRight />
-                </Link>
-              </div>
-
-              <div className={styles.cardBody}>
-                <Image
-                  src={product1}
-                  alt="Product 1"
-                  className={styles.productImage}
-                  priority
-                />
-              </div>
-
+          <Link href="http://shop.akvioo.com" target="_blank" rel="noopener noreferrer" className={styles.shopnav}>
+          <div className={styles.cardWrapper}>
+            <div className={styles.cardHeader}>
+              <span className={styles.productLabel}>Product 1</span>
+              <span className={styles.shopbtn}>Shop Now <FiArrowUpRight /></span>
             </div>
-          </Col>
-
-
-          {/* PRODUCT 2 */}
-          <Col lg={10}>
-            <div className={styles.cardWrapper}>
-
-              <div className={styles.cardHeader}>
-                <span className={styles.productLabel}>Product 2</span>
-
-                <Link href="#" className={styles.arrowBtn}>
-                  <FiArrowUpRight />
-                </Link>
-              </div>
-
-              <div className={styles.cardBody}>
-                <Image
-                  src={product2}
-                  alt="Product 2"
-                  className={styles.productImage}
-                />
-              </div>
-
+            <div className={styles.cardBody}>
+              <Image src={product1} alt="Product 1" className={styles.productImage} priority/>
             </div>
-          </Col>
-
+          </div>
+          </Link>
+           <Link href="http://shop.designdaddys.com" target="_blank" rel="noopener noreferrer" className={styles.shopnav}>
+          <div className={styles.cardWrapper}>
+            <div className={styles.cardHeader}>
+              <span className={styles.productLabel}>Product 2</span>
+              <span className={styles.shopbtn}>Shop Now <FiArrowUpRight /></span>
+            </div>
+            <div className={styles.cardBody}>
+              <Image src={product2} alt="Product 2" className={styles.productImage}/>
+            </div>
+          </div>
+           </Link>
         </Row>
-
       </Container>
     </section>
   );
 };
-
 export default Product;
